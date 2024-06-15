012ba70fa40ec75b53c540dacb9776f7872e457b
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-06-15 12:36:22 -0600

    Change approach for download a bunch of images

--- src/screens/ResizeScreen.tsx
+++ src/screens/ResizeScreen.tsx
@@ -7,46 +7,65 @@ import "./ResizeScreen.css"
 
 export const ResizeScreen = () => {
   const [files, setFiles] = useState<File[]>([])
-  const [filesResized, setFilesResized] = useState<HTMLAnchorElement[]>([])
   const [imageQuality, setImageQuality] = useState<number>(100)
   const [imageSize, setImageSize] = useState<number>(100)
   const [isCompressing, setIsCompressing] = useState<boolean>(false)
 
-  const handleResize = () => {
+  const handleResize = async () => {
     setIsCompressing(true)
-    files.forEach((file) => {
-      resize(file)
-    })
-    filesResized.forEach((image) => {
-      downloadFile(image)
-    })
-    setFilesResized([])
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const image = await resize(file);
+      if (image) await downloadFile(image)
+    }
+
+    // files.forEach(async (file) => {
+    //   const image = await resize(file)
+    //   if (image) downloadFile(image)
+    // })
+
+    // const resizedPromises = files.map((file) => resize(file))
+    // const resizedImages = await Promise.all(resizedPromises)
+    // resizedImages.forEach((image) => {
+    //   if (image) downloadFile(image)
+    // })
     setFiles([])
     setIsCompressing(false)
   }
 
-  const downloadFile = (image: HTMLAnchorElement) => {
-    image.click()
+  const downloadFile = (image: HTMLAnchorElement): Promise<boolean> => {
+    return new Promise((resolve) => {
+      document.body.appendChild(image)
+      image.click()
+      document.body.removeChild(image)
+      resolve(true)
+    })
   }
 
-  const resize = (file: File) => {
-    const image = new Image()
-    image.src = URL.createObjectURL(file)
-    image.onload = () => {
-      const canvas = document.createElement("canvas");
-      const a = document.createElement("a");
-      const ctx = canvas.getContext("2d");
+  const resize = (file: File): Promise<HTMLAnchorElement | null> => {
+    return new Promise((resolve) => {
+      const image = new Image()
+      image.src = URL.createObjectURL(file)
+      image.onload = () => {
+        const canvas = document.createElement("canvas");
+        const a = document.createElement("a");
+        const ctx = canvas.getContext("2d");
+        console.log(file.name)
 
-      canvas.width = image.naturalWidth * (imageSize/100)
-      canvas.height = image.naturalHeight * (imageSize/100)
-      if (ctx) {
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
-        a.href = canvas.toDataURL("image/jpeg", imageQuality/100);
-        a.download = new Date().getTime().toString();
-        setFilesResized((prevFiles) => [...prevFiles, a])
-
+        canvas.width = image.naturalWidth * (imageSize/100)
+        canvas.height = image.naturalHeight * (imageSize/100)
+        if (ctx) {
+          ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+          a.href = canvas.toDataURL("image/jpeg", imageQuality/100);
+          a.download = file.name
+          resolve(a)
+        }else{
+          resolve(null)
+          console.error('Error resizing image', file.name)
+        }
       }
-    }
+    })
   }
 
   return (
