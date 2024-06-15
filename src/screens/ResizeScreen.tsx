bed7c83e2bf269dab70356b8ae145d6d59d4bc03
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-06-15 13:35:49 -0600

    Change download to zip

--- src/screens/ResizeScreen.tsx
+++ src/screens/ResizeScreen.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react"
-import DragAndDrop from "../components/DragAndDrop"
-import RangeSlider from "../components/RangeSlider"
-import { FileList } from "../components/FileList"
+import { useState } from 'react'
+import DragAndDrop from '../components/DragAndDrop'
+import RangeSlider from '../components/RangeSlider'
+import { FileList } from '../components/FileList'
+import JSZip from 'jszip'
+import { saveAs } from 'file-saver'
 
-import "./ResizeScreen.css"
+import './ResizeScreen.css'
+
+interface FileWithBlob {
+  blob: Blob
+  name: string
+}
 
 export const ResizeScreen = () => {
   const [files, setFiles] = useState<File[]>([])
@@ -14,58 +21,61 @@ export const ResizeScreen = () => {
   const handleResize = async () => {
     setIsCompressing(true)
 
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const image = await resize(file);
-      if (image) await downloadFile(image)
-    }
+    const zip = new JSZip();
 
-    // files.forEach(async (file) => {
-    //   const image = await resize(file)
-    //   if (image) downloadFile(image)
-    // })
+    const resizePromises = files.map((file) => resize(file))
+    const resizedImages = await Promise.allSettled(resizePromises)
 
-    // const resizedPromises = files.map((file) => resize(file))
-    // const resizedImages = await Promise.all(resizedPromises)
-    // resizedImages.forEach((image) => {
-    //   if (image) downloadFile(image)
-    // })
+    resizedImages.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        const { blob, name } = result.value
+        zip.file(name, blob)
+      } else {
+        console.error('Error resizing image', result.reason)
+      }
+    })
+
+    // Generate the zip file and download it
+    zip.generateAsync({ type: 'blob' }).then((content) => {
+      saveAs(content, 'resized_images.zip');
+    });
+
     setFiles([])
     setIsCompressing(false)
   }
 
-  const downloadFile = (image: HTMLAnchorElement): Promise<boolean> => {
-    return new Promise((resolve) => {
-      document.body.appendChild(image)
-      image.click()
-      document.body.removeChild(image)
-      resolve(true)
-    })
-  }
-
-  const resize = (file: File): Promise<HTMLAnchorElement | null> => {
-    return new Promise((resolve) => {
+  const resize = (file: File): Promise<FileWithBlob> => {
+    return new Promise((resolve, reject) => {
       const image = new Image()
       image.src = URL.createObjectURL(file)
       image.onload = () => {
         const canvas = document.createElement("canvas");
-        const a = document.createElement("a");
         const ctx = canvas.getContext("2d");
-        console.log(file.name)
 
-        canvas.width = image.naturalWidth * (imageSize/100)
-        canvas.height = image.naturalHeight * (imageSize/100)
+        canvas.width = image.naturalWidth * (imageSize / 100)
+        canvas.height = image.naturalHeight * (imageSize / 100)
         if (ctx) {
           ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
-          a.href = canvas.toDataURL("image/jpeg", imageQuality/100);
-          a.download = file.name
-          resolve(a)
+          canvas.toBlob((blob) => {
+            if (blob) {
+              const fileName = `${file.name.split('.')[0]}_resized.jpg`; // Create a custom file name
+              resolve({ blob, name: fileName });
+            } else {
+              reject({error: 'Error creating blob', name: file.name})
+            }
+          }, 'image/jpeg', imageQuality / 100);
         }else{
-          resolve(null)
-          console.error('Error resizing image', file.name)
+          reject({error: 'Error resizing image', name: file.name})
         }
       }
+      image.onerror = () => {
+        reject({error: 'Error loading image', name: file.name})
+      }
     })
+  }
+
+  if (isCompressing) {
+    return <p>Compressing...</p>
   }
 
   return (
