44bb17f9a72a2d6128e5d9fbafdcb144e1d9808b
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-07-14 11:19:09 -0600

    Create contact sheet page

---
+++ src/screens/ContactSheetScreen.tsx
@@ -0,0 +1,10 @@
+import { ScreenLayout } from "../layouts/ScreenLayout";
+
+
+export default function ContactSheetScreen() {
+  return (
+    <ScreenLayout>
+      <h1>Contact Sheet</h1>
+    </ScreenLayout>
+  )
+}
