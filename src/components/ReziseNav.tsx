3cae31bd9dc1c589de80ebd244d2472f4ac643b7
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-07-15 23:06:08 -0600

    Create nav for navigation through screens

--- src/components/ReziseNav.tsx
+++ src/components/ReziseNav.tsx
@@ -1,3 +1,4 @@
+import { Link } from "wouter"
 import { ResizeItLogo } from "./Icons/ResizeITLogo"
 
 import './ResizeNav.css'
@@ -5,10 +6,19 @@ import './ResizeNav.css'
 export const ResizeNav = () => {
   return (
     <div className="nav-container">
+
       <div className="logo">
         <ResizeItLogo fillColor='#6dc24b' />
+        <h1>Resize<span>IT</span></h1>
       </div>
-      <h1>Resize<span>IT</span></h1>
+      <ul>
+        <li>
+          <Link href="/">Resize</Link>
+        </li>
+        <li>
+          <a href="/contact-sheet">Contact Sheet</a>
+        </li>
+      </ul>
     </div>
   )
 }
