4439bcc361e61b54c07898f7661704e604ffccc9
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-07-18 16:47:06 -0600

    Fix link navogation and set active nav element

--- src/components/ReziseNav.tsx
+++ src/components/ReziseNav.tsx
@@ -4,19 +4,29 @@ import { ResizeItLogo } from "./Icons/ResizeITLogo"
 import './ResizeNav.css'
 
 export const ResizeNav = () => {
+
   return (
     <div className="nav-container">
-
       <div className="logo">
         <ResizeItLogo fillColor='#6dc24b' />
         <h1>Resize<span>IT</span></h1>
       </div>
       <ul>
         <li>
-          <Link href="/">Resize</Link>
+          <Link
+            to="/"
+            className={(active) => active ? "active": ""}
+          >
+            Resize
+          </Link>
         </li>
         <li>
-          <a href="/contact-sheet">Contact Sheet</a>
+          <Link
+            to="/contact-sheet"
+            className={(active) => active ? "active": ""}
+          >
+            Contact Sheet
+          </Link>
         </li>
       </ul>
     </div>
